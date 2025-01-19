fdbd0f3241a6ae086eb4a4ea604851914ae46c26
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-19 13:20:31 +0330

    js1

---
+++ script.js
@@ -0,0 +1,4 @@
+let form=document.getElementById('usernameForm');
+let username=document.getElementById('username');
+let errorText=document.getElementById('errorMessage');
+let button=document.querySelector('button');
