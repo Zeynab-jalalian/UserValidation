43974f95549393567af132db5f71172645a7a355
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-19 13:32:40 +0330

    regex

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ let username=document.getElementById('username');
 let errorText=document.getElementById('errorMessage');
 let button=document.querySelector('button');
 
-button.addEventListener('click',function(){
+form.addEventListener('submit',function(event){
     event.preventDefault();
+
+    let rule=/^[a-zA-Z]+[a-zA-Z0-9]{5,}$/ig;
+    if(username.value.match(rule)){
+    errorText.style.display="block"
+    errorText.innerText="valid username!"
+    errorText.style.color="green"
+    }else{
+  errorText.style.display="block"
+   errorText.style.color="red"
+    }
 })
