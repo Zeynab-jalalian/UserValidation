bdf792b8a204ffa1fb8af1ed905dd3c253941369
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-19 13:22:03 +0330

    preventDefault()

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let form=document.getElementById('usernameForm');
 let username=document.getElementById('username');
 let errorText=document.getElementById('errorMessage');
 let button=document.querySelector('button');
+
+button.addEventListener('click',function(){
+    event.preventDefault();
+})
